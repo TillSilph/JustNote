2085e416007cac78b6579acfda0578eba19d930b
TillSilph <95415584+TillSilph@users.noreply.github.com>
2025-03-04 16:12:16 +0100

    feat(SJ_Note-5): notification processing migrate

--- src/note/note.module.ts
+++ src/note/note.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { NoteService } from './note.service';
 import { NoteController } from './note.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { KafkaModule } from 'src/kafka/kafka.module';
 
 @Module({
+  imports:[KafkaModule],
   controllers: [NoteController],
   providers: [NoteService, PrismaService],
 })
--- src/note/note.service.ts
+++ src/note/note.service.ts
@@ -1,40 +1,58 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
-import { Prisma } from '@prisma/client';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
+import { Prisma } from "@prisma/client";
+import { KafkaService } from "src/kafka/kafka.service";
 
 @Injectable()
 export class NoteService {
-  constructor(private prisma: PrismaService) {}
+    constructor(
+        private prisma: PrismaService,
+        private readonly kafka: KafkaService
+    ) {}
 
-  // Получение всех записей
-  async getAllNotes() {
-    const allNotes = await this.prisma.note.findMany();
-    return {
-      completed: allNotes.filter((el) => el.completed),
-      inWork: allNotes.filter((el) => !el.completed),
-    };
-  }
+    // Получение всех записей
+    async getAllNotes() {
+        const allNotes = await this.prisma.note.findMany();
+        return {
+            completed: allNotes.filter((el) => el.completed),
+            inWork: allNotes.filter((el) => !el.completed),
+        };
+    }
 
-  // Получение одной заметки по ID
-  async getNoteById(id: string) {
-    return this.prisma.note.findUnique({ where: { id } });
-  }
+    // Получение одной заметки по ID
+    async getNoteById(id: string) {
+        return this.prisma.note.findUnique({ where: { id } });
+    }
 
-  // Создание новой заметки
-  async createNote(data: Prisma.NoteCreateInput) {
-    return this.prisma.note.create({ data });
-  }
+    // Создание новой заметки
+    async createNote(data: Prisma.NoteCreateInput) {
+        const note = await this.prisma.note.create({ data });
+        if (data.notificationTime) {
+            this.kafka.sendNotification(
+                JSON.stringify({
+                    id: note.id,
+                    notificationTime: note.notificationTime,
+                })
+            );
+        }
+        return note;
+    }
 
-  // Редактирование заметки
-  async updateNote(id: string, data: Prisma.NoteUpdateInput) {
-    return this.prisma.note.update({
-      where: { id },
-      data,
-    });
-  }
+    // Редактирование заметки
+    async updateNote(id: string, data: Prisma.NoteUpdateInput) {
+        if (data.notificationTime) {
+            this.kafka.sendNotification(
+                JSON.stringify({ id, notificationTime: data.notificationTime })
+            );
+        }
+        return this.prisma.note.update({
+            where: { id },
+            data,
+        });
+    }
 
-  // Удаление заметки
-  async deleteNote(id: string) {
-    return this.prisma.note.delete({ where: { id } });
-  }
+    // Удаление заметки
+    async deleteNote(id: string) {
+        return this.prisma.note.delete({ where: { id } });
+    }
 }
